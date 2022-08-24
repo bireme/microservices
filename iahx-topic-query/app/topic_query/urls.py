eb358ead55119faff4e973f3c2f53ce571a0ca61
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2022-08-24 11:46:24 -0300

    Redireciona página inicial para /api/

--- iahx-topic-query/app/topic_query/urls.py
+++ iahx-topic-query/app/topic_query/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework import routers
 
 from main import views as api_views
@@ -11,5 +12,8 @@ admin.site.site_header = 'IAHX Topic Query'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+
+    # redirect first page for /api/
+    path('', RedirectView.as_view(url='/api/', permanent=True))
 ]
